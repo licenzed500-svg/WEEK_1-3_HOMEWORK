0eba993d5fae05bfc4409c9cfc90e0f723a1d63c
licenzed500-svg <79693216+licenzed500-svg@users.noreply.github.com>
2022-10-13 21:08:33 +0300

    Create Elipse_S.cpp

---
+++ Elipse_S.cpp
@@ -0,0 +1,41 @@
+//8b
+
+#include <iostream>
+#include <cmath>
+
+#define M_pi 3.14
+
+float elipseS(float rad, float axleShaft_a, float axleshaft_b)
+{
+    float result = M_pi * axleShaft_a * axleshaft_b;
+    return result;
+}
+
+void data_out(float result)
+{
+    std::cout << "result is:" << result;
+    std::cout << "\n";
+}
+void __program()
+{
+    float rad = 0;
+    float axleShaft_a = 0;
+    float axleShaft_b = 0;
+    float result = 0;
+    std::cout << "enter radius:";
+    std::cin >> rad;
+    std::cout << "\n";
+    std::cout << "enter axle shaft a:";
+    std::cin >> axleShaft_a;
+    std::cout << "\n";
+    std::cout << "enter axle shaft b:";
+    std::cin >> axleShaft_b;
+    std::cout << "\n";
+    result = elipseS(rad, axleShaft_a, axleShaft_b);
+    data_out(result);
+}
+int main()
+{
+    __program();
+}
+
